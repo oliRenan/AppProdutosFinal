a19c5164a6b71443c63ffec544e4ec1ff7c7082d
oliRenan <oliveirarenan479@gmail.com>
2025-06-09 09:12:17 -0300

    add fold structure

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Renan</Text>
+      <Text>Teste</Text>
       <StatusBar style="dark" />
     </View>
   );
